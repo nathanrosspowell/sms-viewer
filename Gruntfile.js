2b0a9d132aa914f735d5dc58bb68895b36bf3372
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-01-06 00:14:20 -0500

    Added live reload :repeat:

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,51 @@
 module.exports = function(grunt) {
-  var build = 'build/sms-veiwer/';
-  var temp = 'temp/';
+
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+ 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    config: {
+      src: 'src',
+      dist: 'dist/sms-viewer',
+      temp: 'temp'
+    },
+
+    watch: {
+      build: {
+        files: ['<%= config.src %>/**/*.{html,css}'],
+        tasks: ['build']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          // Not looking for jQuery changes for optim
+          '<%= config.dist %>/{,*/}*.html',
+          '<%= config.dist %>/css/{,*/}*.css'
+        ]
+      }
+    },
+
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '<%= config.dist %>'
+          ]
+        }
+      }
+    },
+
     replace: {
       sms: {
         options: {
@@ -25,49 +68,79 @@ module.exports = function(grunt) {
                 '**/*',
                 '!**/*.swp'
             ],
-            dest: temp
+            dest: '<%= config.temp %>'
           }
-        ],
+        ]
+        ,
       }
     },
+
     copy: {
       sms: {
         files: [
           {
             expand: true,
-            cwd: "temp/",
+            cwd: '<%= config.temp %>/',
             src: [
               '**/*',
               '!**/*.swp'
             ],
-            dest: build,
+            dest: '<%= config.dist %>',
             filter: "isFile"
           },
           {
             expand: true,
-            cwd: "bower_components/",
+            cwd: 'bower_components/',
             src: [
               '**/*',
             ],
-            dest: build
+            dest: '<%= config.dist %>'
           }
         ]
       }
     },
+
     'gh-pages': {
         sms: {
           options: {
-            base: build,
+            base: '<%= config.dist %>',
             branch: 'gh-pages',
             message: 'Grunt deploy <%= grunt.template.today() %>'
           },
           src: ['**']
         }
-    }
+    },
+
+    clean: [
+      '<%= config.dist %>/**/*.*',
+      '<%= config.temp %>/**/*.*',
+    ]
+
   });
+
   grunt.loadNpmTasks('grunt-replace');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-gh-pages');
-  grunt.registerTask('default', ['replace', 'copy']);
-  grunt.registerTask('deploy', ['default', 'gh-pages']);
+
+  grunt.registerTask('build', [
+      'replace', 
+      'copy'
+  ]);
+
+  grunt.registerTask('deploy', [
+      'clean',
+      'default', 
+      'gh-pages' 
+  ]);
+
+  grunt.registerTask('server', [
+    'build',
+    'connect:livereload',
+    'watch'
+  ]);
+
+  grunt.registerTask('default', [
+    'build'
+  ]);
+
 };
